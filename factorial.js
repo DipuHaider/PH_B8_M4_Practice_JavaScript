b8c7633c2d75f9482fdc66d78019b47bc28704fd
Dipu Haider <dipuhaider@gmail.com>
2023-08-08 14:25:50 +0600

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,37 @@
+// function factorial(number) {
+//   let result = 1;
+//   for (let i = 1; i <= number; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+
+// const result = factorial(9);
+// console.log(result);
+
+// function factorial(number) {
+//   let result = 1;
+//   for (let i = number; i >= 1; i--) {
+//     result = result * i;
+//     console.log(i);
+//   }
+//   return result;
+// }
+
+// const number = 9;
+// const fact = factorial(number);
+// console.log(number, fact);
+
 function factorial(number) {
+  let num = 1;
   let result = 1;
-  for (let i = 1; i <= number; i++) {
-    result = result * i;
+  while (num <= number) {
+    result = result * num;
+    num++;
   }
   return result;
 }
 
-const result = factorial(9);
-console.log(result);
+const number = 9;
+const fact = factorial(number);
+console.log(number, fact);
